d3ffff47e510c23e0455287a5b94abecb89bbb2a
Brian Foo <brian@youaremyjoy.org>
2014-02-11 17:37:32 -0500

    init layout with swipe navigation

--- www/js/app.js
+++ www/js/app.js
@@ -22,17 +22,59 @@
       // Wait for device to be ready
       } else {
         document.addEventListener('deviceready', function(){
-          that.onDeviceReady()
+          that.onDeviceReady();
         }, false);
       }
       
     };
     
+    App.prototype.initPages = function(){
+      // retrieve last page from web storage
+      var i = localStorage.page_i || 0;
+      this.$pages = $('.page');
+      this.goToPage(parseInt(i));
+    };
+    
+    App.prototype.initSwipes = function(){
+      var that = this;
+      Hammer($('body')[0]).on('swiperight', function(e) {
+        that.goToPagePrev();
+      });
+      Hammer($('body')[0]).on('swipeleft', function(e) {
+        that.goToPageNext();
+      });
+    };
+    
+    App.prototype.goToPage = function(i){
+      this.options.debug && console.log('Going to page '+i);      
+      this.current_page_i = i;
+      if ( this.$page ) this.$page.removeClass('active');
+      this.$page = this.$pages.eq(i);
+      this.$page.addClass('active');
+      // store in web storage
+      localStorage.page_i = i;
+    };
+    
+    App.prototype.goToPageNext = function(){
+      var i = this.current_page_i + 1;
+      if (i < this.$pages.length) {
+        this.goToPage(i);
+      } else {
+        this.options.debug && console.log('End');
+      }
+    };
+    
+    App.prototype.goToPagePrev = function(){
+      var i = this.current_page_i - 1;
+      if (i >= 0) {
+        this.goToPage(i);
+      }
+    };
+    
     App.prototype.onDeviceReady = function(){
-      this.options.debug && console.log('Device Ready!');
-      
-      $('#app').addClass('loaded');
-      
+      this.options.debug && console.log('Device Ready!');      
+      this.initPages();
+      this.initSwipes();
     };
 
     return App;
